04ec6f69a89824d2f255369ba5ea81fb181ae864
Roman Chesnokov <66942939+chessnocow@users.noreply.github.com>
2022-10-11 17:56:20 +0200

    Add README.md

--- tweeter_stat/main.py
+++ tweeter_stat/main.py
@@ -13,11 +13,11 @@ def main(query):
     Get and print Twitter metrics.
 
     :param query: query to search
-    :return: None
+    :return: Nonepipi
     """
     client = get_twitter_client()
     start_time = datetime.utcnow() - timedelta(minutes=15)
-    last_check_time = datetime.utcnow()
+    last_check_time = datetime.utcnow() - timedelta(minutes=15)
     while True:
         if datetime.utcnow() - start_time > timedelta(seconds=REFRESH_API_DATA_SECONDS):
             tweets_df = get_tweets_df(client, query, start_time)
